d3b0d41ec8b818a5be1c495179922a462e5a78a3
= <geison.sn@gmail.com>
2015-09-10 16:58:21 -0400

    Ajustes no formuário de cadastro de reclamação e ajustes na action bar do cadastro de reclamação

--- app/src/main/java/br/com/gsn/sysbusapp/activity/NovaReclamacaoActivity.java
+++ app/src/main/java/br/com/gsn/sysbusapp/activity/NovaReclamacaoActivity.java
@@ -1,11 +1,13 @@
 package br.com.gsn.sysbusapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 import br.com.gsn.sysbusapp.R;
 
@@ -13,6 +15,9 @@ import br.com.gsn.sysbusapp.R;
  * Created by Geison on 09/09/2015.
  */
 public class NovaReclamacaoActivity extends AppCompatActivity {
+
+    private MenuItem menuItemProgressBar;
+    private ProgressBar progressBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +27,8 @@ public class NovaReclamacaoActivity extends AppCompatActivity {
 
         setSupportActionBar(mToolbar);
 
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setTitle("Reclamar");
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
     }
@@ -34,12 +40,39 @@ public class NovaReclamacaoActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        menuItemProgressBar = menu.findItem(R.id.action_progresso);
+//        ProgressBar viewById = (ProgressBar) findViewById(item.getItemId());
+//        progressBar = (ProgressBar) MenuItemCompat.getActionView(item);
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         if (item.getItemId() == android.R.id.home) {
+//            NavUtils.navigateUpFromSameTask(this);
+            NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
+        }
+
+        if (item.getItemId() == R.id.action_save_reclamacao) {
             NavUtils.navigateUpFromSameTask(this);
         }
 
+        if (item.getItemId() == R.id.action_recarregar) {
+            showProgressBar();
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showProgressBar() {
+        menuItemProgressBar.setVisible(true);
+    }
+
+    private void HideProgressBar() {
+        menuItemProgressBar.setVisible(false);
     }
 }
